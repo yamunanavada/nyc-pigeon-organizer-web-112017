e9c0ab7dbcb71465c996033f7a483de2d870d439
Yamuna Navada <yamunanavada@gmail.com>
2017-11-06 23:57:17 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,47 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  def nyc_pigeon_data(hash)
+  new_data = {}
+  keys = hash.keys
+  
+  #find pigeon names
+  pigeon_names = []
+  hash.each do |char, what|
+    what.each do |type, pigeons|
+      pigeon_names.push(pigeons)
+    end
+  end
+  
+  pigeon_names = pigeon_names.flatten.uniq
+
+  #start the new_data hash
+  pigeon_names.each do |name|
+    new_data[name] = {}
+  end
+  
+  #push the stuff into each
+  pigeon_names.each do |name|
+    hash.each do |char, what|
+     what.each do |type, pigeons|
+       pigeons.each do |who|
+          if name == who && !new_data[name][char]
+            new_type = type.to_s
+            new_data[name][char] = [new_type]
+          elsif name == who
+            new_data[name][char] << type.to_s
+          end
+        end
+      end
+    end
+  end
+  
+  new_data
+       
+      
+
+
+end
+
+  
+  
 end
